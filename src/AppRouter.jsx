3ba6f48dc1a796a4822e46d3fc0b6d2f4801d7e0
Carmelo Mayen <carmelo5mayen@gmail.com>
2023-08-25 22:58:22 -0600

    final commit

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage, PokemonPage, SearchPage } from './pages';
+import { Navigation } from './components/Navigation';
 
 export const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<HomePage />} />
+        <Route path='pokemon/:id' element={<PokemonPage />} />
+        <Route path='search' element={<SearchPage />} />
+      </Route>
+
+      <Route path='*' element={<Navigate to='/' />} />
+      
+    </Routes>
   )
 }
